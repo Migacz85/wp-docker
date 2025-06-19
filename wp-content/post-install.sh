e188965fe482bab58fe538def5ced514da576970
Migacz85 <migac85@gmail.com>
2025-06-19 19:10:33 +0100

    chore: Update post-install script to install curl and WP-CLI

--- wp-content/post-install.sh
+++ wp-content/post-install.sh
@@ -3,7 +3,11 @@ set -e
 
 echo "Running WordPress post-installation script..."
 
+apt update && apt install curl &&
 wp theme install twentytwentyfour --activate --allow-root
 
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
+    chmod +x wp-cli.phar && \
+    mv wp-cli.phar /usr/local/bin/wp
 
 echo "Post-installation complete!"
